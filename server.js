ab8fa67a6212abdd22341aa73b125368f4351e63
esben holk nielsen <esbenholknielsen@gmail.com>
2022-09-13 17:02:09 +0200

    new style for boid project

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(cors());
 // app.use(router);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
